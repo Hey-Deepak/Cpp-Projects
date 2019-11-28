22c3cac4a3da171eacf9aa9da3686694b0529325
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-28 22:28:18 +0530

    math function added

---
+++ Dc7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int x = 4;
+    int y = 6;
+    cout << min(x, y)<<endl;
+    cout << max(x, y)<<endl;
+    cout << round(2.66);
+
+}
