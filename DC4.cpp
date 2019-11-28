941febad0b622ca725082026e5b51dedbab39cfe
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-28 22:12:42 +0530

    Length of String

---
+++ DC4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    cout<<"Enter Your name :- \n";
+    string name ;
+    cin>>name;
+    cout<<"Name :- "<< name<<"\nLength of String is " << name.length();
+    return 0;
+}
