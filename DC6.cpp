ea3543ef2f3c8b6f6f3a8744a3a29fe0174aaa2f
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-28 22:21:07 +0530

    String Concatenation

---
+++ DC6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter First Name :- \n";
+    string name, lastName ;
+    cin>>name;
+    cout<<"Enter Last Name :- \n";
+    cin>>lastName;
+    cout<<"\nName :- "<< name + lastName ;
+    return 0;
+}
