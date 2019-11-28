f2d29b519ab6c020499485b1b8bf8ab1135fe081
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-29 02:51:30 +0530

    switch case added

---
+++ DC11.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int day = 2;
+    switch (day)
+    {
+    case 1:
+        cout<<"One";
+        break;
+    case 2:
+        cout<<"Two";
+        break;
+    case 3:
+        cout<<"three";
+        break;
+    case 4:
+        cout<<"four";
+        break;
+    
+    default:cout<<"Bas Kar Re ab..";
+        break;
+    }
+    return 0;
+}
