097abe6a5014bf40078be16159bdcaf9a1311c70
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-28 22:34:17 +0530

    boolean Expression added

---
+++ DC8.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool CppIsFun = true;
+    bool CppIsHeadache = false;
+    cout<<CppIsFun<<endl;
+    cout<<CppIsHeadache<<endl;
+    return 0;
+}
