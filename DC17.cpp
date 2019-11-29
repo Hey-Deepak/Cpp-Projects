347ecf6b7386b047d3f35c8d2bae5ecf38cb403b
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-29 19:07:45 +0530

    Loop through an array

---
+++ DC17.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name[] = {"Deepak", "DC", "Dfuck", "Hey-Deepak"};
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<name[i]<<endl;  
+    }
+    
+    return 0;
+}
