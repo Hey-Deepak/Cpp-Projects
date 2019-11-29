3fa03be71b1bf8e530a5d1a146fc9a1d9aed2256
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-29 19:56:29 +0530

    function added

---
+++ DC21.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void myFunction();
+int main(){
+    myFunction();    
+    myFunction();    
+    myFunction();    
+    return 0;
+}
+void myFunction() {
+    cout<<"I am Function"<<endl;
+}
