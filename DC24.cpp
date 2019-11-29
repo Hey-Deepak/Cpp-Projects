0c77629357f5e9d94458156f5bae5583fcacd5c1
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-29 20:37:37 +0530

    Function overloading added

--- DC24.cpp
+++ DC24.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int myFunction(int x, int y);
 double myFunction(double x, double y);
 int main(){
-    cout<<myFunction(5 , 4)<<endl;
-    cout<<myFunction(5.555 , 4.555)<<endl;
+    cout<<"Int :-"<<myFunction(5 , 4)<<endl;
+    cout<<"Double :-"<<myFunction(5.555 , 4.555)<<endl;
     return 0;
 }
 int myFunction(int x, int y){
