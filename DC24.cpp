95247969fa3580d83660d260cb370d63e8c43ac1
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-29 20:36:04 +0530

    function overloading

---
+++ DC24.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int myFunction(int x, int y);
+double myFunction(double x, double y);
+int main(){
+    cout<<myFunction(5 , 4)<<endl;
+    cout<<myFunction(5.555 , 4.555)<<endl;
+    return 0;
+}
+int myFunction(int x, int y){
+    return (x + y);
+}
+double myFunction(double x, double y){
+    return (x + y);
+}
