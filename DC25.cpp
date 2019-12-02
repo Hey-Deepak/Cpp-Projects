e0c34647c052d2436d47b4c5c42afee2d86e965e
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-02 23:56:22 +0530

    Update DC25.cpp

--- DC25.cpp
+++ DC25.cpp
@@ -1,18 +1,25 @@
-class MyClass {       // The class
-  public:             // Access specifier
-    int myNum;        // Attribute (int variable)
-    string myString;  // Attribute (string variable)
+class Car {
+  public:
+    string brand;   
+    string model;
+    int year;
 };
 
 int main() {
-  MyClass myObj;  // Create an object of MyClass
+  // Create an object of Car
+  Car carObj1;
+  carObj1.brand = "BMW";
+  carObj1.model = "X5";
+  carObj1.year = 1999;
 
-  // Access attributes and set values
-  myObj.myNum = 15; 
-  myObj.myString = "Some text";
+  // Create another object of Car
+  Car carObj2;
+  carObj2.brand = "Ford";
+  carObj2.model = "Mustang";
+  carObj2.year = 1969;
 
   // Print attribute values
-  cout << myObj.myNum << "\n";
-  cout << myObj.myString;
+  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
   return 0;
 }
