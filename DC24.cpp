11172360c8002499b48fecae08a61b9560f6863c
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-03 20:48:59 +0530

    class object created

--- DC24.cpp
+++ DC24.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
-int myFunction(int x, int y);
-double myFunction(double x, double y);
+class MyClass {
+    public :
+    int age;
+    string name;
+};
 int main(){
-    cout<<"Int :-"<<myFunction(5 , 4)<<endl;
-    cout<<"Double :-"<<myFunction(5.555 , 4.555)<<endl;
+    MyClass myAge;
+    MyClass myName;
+    myAge.age = 21;
+    myName.name = "Deepak";
+    cout<< myAge.age << " " << myName.name <<endl;
     return 0;
-}
-int myFunction(int x, int y){
-    return (x + y);
-}
-double myFunction(double x, double y){
-    return (x + y);
 }
