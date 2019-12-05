a6b8643523d42ffe86774785a9e21ea0cf5ad8cd
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-05 23:19:33 +0530

    Update MarksCalculater.cpp

--- MarksCalculater.cpp
+++ MarksCalculater.cpp
@@ -17,9 +17,15 @@ int main(){
     cin>>attendance;
     total = (firstInSem*30)/100 + (secondInSem*30)/100 + (finalSem*50)/100 + (assignment) + (attendance);
     cout<<"Your Total Marks :- "<< total << endl;
-    if (total >= 35)
+    if (total >= 35 )
+    {   if (finalSem >= 35)
     {
         cout<<"You are Safe and you crossed Danger Zone, Your extra marks is " << total - 35 << endl;
+    }else
+    {
+        cout<<"You are UnSafe and you have to gain " << (35 - finalSem) << " more marks.\nMeans Next time in Final Back Paper you have to score Only 35 Marks" << endl;
+    }
+    
     } else
         cout<<"You are UnSafe and you have to gain " << (35 - total)*2 << " more marks.\n Means Next time in Final Back Paper you have to score " << (finalSem + (35 - total)*2) <<endl;
     {
