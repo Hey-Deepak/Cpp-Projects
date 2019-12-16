d8975476122d960ab408abffd524752d132beb87
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-16 07:22:50 +0530

    matrix

---
+++ DC39.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int matrix[100][100] ;
+    cout << "Enter Hight of N * N matrix" << endl;
+    cin >> n ;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j] ;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " " ;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
