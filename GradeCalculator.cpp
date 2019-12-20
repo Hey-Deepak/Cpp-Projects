3e13a09af857fed2a931d7a80bb7a025d09412ff
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-20 23:24:31 +0530

    cal

---
+++ GradeCalculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<array>
+
+using namespace std;
+
+int main(){
+    array<char,3> arr[] = {'A', 'B', 'C'};
+    for (int i = 0; i < arr.size(); i++)
+    {
+        printf("%d\n", arr[i] - 55);
+    }
+        
+    return 0;
+}
