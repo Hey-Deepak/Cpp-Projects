c75c39c9f6694f0196b9d8bf1baae1a6fa25330d
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-22 18:35:26 +0530

    Update DC42.cpp

--- DC42.cpp
+++ DC42.cpp
@@ -1,30 +1,19 @@
 #include<iostream>
 using namespace std;
+void strcpy(int* arr1, int *arr2);
 
 int main(){
-    int flag = 0;
-    int arr1[] = {1, 2, 3, 4, 5,6, 7, 8, 88};
-    int arr2[] = {1, 2, 3, 4, 5,6, 7, 8, 9, 0};
-    for (int i = 0; i < 10; i++)
-    {   
-        if (arr2[i] == arr1[i])
-        {
-            flag = 1;
-        } else
-        {
-            flag = 0;
-        }
-    }
+    int arr1[] = {1, 2, 3, 4, 5,6, 7, 8};
+    int arr2[];
 
-    if (flag == 1)
-    {
-        cout << "Equal" << endl;
-    } else
-    {
-        cout << "Unequal" << endl;  
-    }
-    
-    
+   
     
     return 0;
+}
+void strcpy(int *arr1, int *arr2){
+    if ((*arr2 = *arr1) != '\0')
+    {
+        
+    }
+    
 }
