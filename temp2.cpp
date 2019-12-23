602ff04103005430ecde89c6a8d69c20cb85fe9c
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-23 14:56:05 +0530

    Update temp2.cpp

--- temp2.cpp
+++ temp2.cpp
@@ -5,7 +5,11 @@ int main(){
     int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int* p = &a[0];
     cout << *p << endl;
-    *p++;
+    p++;
     cout << *p << endl;
+    (p+2);
+    p++;
+    cout << *(p - 2) << endl;
+
     return 0;
 }
