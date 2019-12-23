eb09378af1577ba6a556008eb3dd1f26ee866788
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-23 22:06:16 +0530

    Update DC43.cpp

--- DC43.cpp
+++ DC43.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 void swap(int*, int*);
+
 int main(){
     int a = 2, b = 4;
     swap(&a, &b);
     cout << a << b << endl;    
     return 0;
 }
+
 void swap(int* x, int* y){
     int temp;
     temp = *x;
