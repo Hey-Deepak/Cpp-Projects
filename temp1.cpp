cc2971cbf1baf8dbb7ab8fb5201b917bf2429cb0
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-23 12:46:13 +0530

    Update temp1.cpp

--- temp1.cpp
+++ temp1.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-int *findMid(int a[], int lent);
+int findMid(int a[], int lent);
 
 int main(){
     int a[] = {1, 2, 3, 4, 5};
     int len = sizeof(a)/sizeof(a[0]);
-    int* mid = findMid(a, len);
-    cout << *mid << endl;    
+    int mid = findMid(a, len);
+    cout << mid << endl;    
     return 0;
 }
-int *findMid(int a[], int len){
-    return &a[len/2];
+int findMid(int a[], int len){
+    return a[len/2];
 }
