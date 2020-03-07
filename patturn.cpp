ca3999972c04b3da172a80a1d54e85ef7a4c3889
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-03-07 22:40:49 +0530

    Update patturn.cpp

--- patturn.cpp
+++ patturn.cpp
@@ -9,11 +9,22 @@ int main()
     {
         if (i <= n)
         {
-            for (int j = 0; j < n - 1; j++)
+            for (int j = 0; j < n - i; j++)
             {
                 cout << "  " ;
             }
             for (int k = 0; k < i; k++)
+            {
+                cout << "*" ;
+            }
+            printf("\n");
+        } else {
+            
+            for (int j = 0; j < n; j++)
+            {
+                cout << "  " ;
+            }
+            for (int k = n; k < n*2 ; k--)
             {
                 cout << "*" ;
             }
