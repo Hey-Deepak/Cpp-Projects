990736d1795f57a0b5aa2994583ce4e75f7193d8
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-03-13 23:37:00 +0530

    Update Exp.cpp

--- Exp.cpp
+++ Exp.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-  cout << "Hello World";
+
+int main(){
+    int array[] = {2, 4, 6, 1, 5};
+    for (int j = 0; j < 6 ; j++)
+    {
+        cout << "Hi" << endl;
+        cout << array[j] << endl;
+    }
+    
+    return 0;
 }
