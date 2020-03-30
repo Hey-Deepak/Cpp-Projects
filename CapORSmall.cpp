b358e8818fc2b18a5f2b4f714aa8e283db05bab2
Hey-Deepak <choudharydeepak990@gmail.com>
2020-03-30 14:17:16 +0530

    Create CapORSmall.cpp

---
+++ CapORSmall.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char ch;
+    cin >> ch;
+    
+    printf("%d\n", ch);
+    if (64 > ch || ch < 91)
+    {
+        cout << "1" << endl;
+    }
+    else if( 96 > ch || ch < 123)    
+    {
+        cout << "0" << endl;
+    }else
+    {
+        cout << "-1" << endl;    // Bug
+    }
+
+
+    exp do not take it serious.
+    return 0;
+}
