b471350614868e5fc4711f0b96f666e264562d4b
Hey-Deepak <choudharydeepak990@gmail.com>
2020-04-07 16:17:52 +0530

    Create WaterMalon.cpp

---
+++ WaterMalon.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+    if (input == 2)
+    {
+        printf("NO");
+    }else if (input % 2 == 0)
+    {
+        printf("YES");
+    } els
