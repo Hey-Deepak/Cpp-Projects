5df8471e9dd8c608f543d343e8a02fc6b3b3604f
Hey-Deepak <choudharydeepak990@gmail.com>
2020-12-11 17:23:48 +0530

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n = 16;
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            for (int k = 0; k < 10; k++){
+                for (int l = 0; l < 10; l++){
+
+                    if (n == i + j + k + l)
+                    {
+                        cout << i << "+" << j <<"+"<< k <<"+"<< l<< endl;
+                    }
+                        
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
